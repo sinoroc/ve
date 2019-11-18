61b051b93e06a69be700d729edbb37f34243016e
sinoroc <sinoroc.code+git@gmail.com>
2019-11-18 16:16:22 +0100

    Add help message for CLI option 'directory'

--- src/ve/cli.py
+++ src/ve/cli.py
@@ -8,8 +8,12 @@
 import argparse
 import logging
 
-from . import _meta
 from . import _core
+from . import _i18n
+from . import _meta
+
+
+_ = _i18n._
 
 
 def main():
@@ -22,7 +26,14 @@ def main():
         allow_abbrev=False,
     )
     parser.add_argument('--version', action='version', version=_meta.VERSION)
-    parser.add_argument('directory', default='.venv', nargs='?')
+    parser.add_argument(
+        'directory',
+        default='.venv',
+        help=_(
+            "create the virtual environment in this directory (default .venv)",
+        ),
+        nargs='?',
+    )
     #
     args = parser.parse_args()
     logger.info("Arguments %s", args)
